03156b6764cb448e581bd44ebba4022b41f91055
Seoyeong Lee <66051416+leeseooo@users.noreply.github.com>
2022-06-17 18:14:11 +0900

    연습문제

---
+++ Level 2/올바른 괄호.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char>st;
+    for(int i=0;i<s.size();i++){
+        if(s[i] == '('){
+            st.push(s[i]);
+        }else{
+            if(!st.empty() && st.top() == '(')st.pop();
+            else return false;
+        }
+    }
+    if(st.empty())return true;
+    return false;
+}
